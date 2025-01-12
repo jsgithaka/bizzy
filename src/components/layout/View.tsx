d5147cd2525566d9cb6c6b6c7fd01980e21c03b1
jsgithaka <jsgithaka@gmail.com>
2025-01-12 14:44:46 +0300

    feat(components/layout): create and style `View` component
    Objective(s):
    1. Create `View` component - this component will be used to construct sections.
    
    Highlight(s):
    1. Created `View` component.
    2. Exported component.

---
+++ src/components/layout/View.tsx
@@ -0,0 +1,19 @@
+import { ViewStyles } from '@layoutStyles';
+import { ReactNode } from 'react';
+
+interface ViewProps {
+    id: string;
+    styles: string;
+    children?: ReactNode;
+}
+
+export default function View({ id, styles, children }: ViewProps) {
+    return (
+        <section
+            id={id}
+            className={[ViewStyles.View, styles].join(' ')}
+        >
+            {children}
+        </section>
+    );
+}
