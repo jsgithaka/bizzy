c06231bb0821258e1229e05348a5ab7f51a83adf
jsgithaka <jsgithaka@gmail.com>
2025-01-28 15:48:43 +0300

    feat(components/structural): create and export `Aside` component and styles

--- src/exports/components/structural.ts
+++ src/exports/components/structural.ts
@@ -1,2 +1,3 @@
 export { default as Header } from '@/components/structural/Header/Header';
 export { default as Main } from '@/components/structural/Main/Main';
+export { default as Aside } from '@/components/structural/Aside/Aside';
--- src/exports/styles/structural.ts
+++ src/exports/styles/structural.ts
@@ -1,2 +1,3 @@
 export { default as HeaderStyles } from '@/styles/structural/Header/Header.module.css';
 export { default as MainStyles } from '@/styles/structural/Main/Main.module.css';
+export { default as AsideStyles } from '@/styles/structural/Aside/Aside.module.css';
