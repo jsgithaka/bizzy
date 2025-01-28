244cbcfd704962198e02332e103b9c17c2d7b5b1
jsgithaka <jsgithaka@gmail.com>
2025-01-28 13:46:40 +0300

    feat(exports): export `Main` component and corresponding style module

--- src/exports/components/structural.ts
+++ src/exports/components/structural.ts
@@ -1,1 +1,2 @@
 export { default as Header } from '@/components/structural/Header/Header';
+export { default as Main } from '@/components/structural/Main/Main';
--- src/exports/styles/structural.ts
+++ src/exports/styles/structural.ts
@@ -1,1 +1,2 @@
 export { default as HeaderStyles } from '@/styles/structural/Header/Header.module.css';
+export { default as MainStyles } from '@/styles/structural/Main/Main.module.css';
