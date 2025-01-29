2c11810a05766309e475133bde2c7188f9761ac6
jsgithaka <jsgithaka@gmail.com>
2025-01-29 08:42:53 +0300

    feat(components/widgets): create `NavigationLarge` and `NavigationSmall` components' style modules

---
+++ src/exports/styles/widgets.ts
@@ -0,0 +1,2 @@
+export { default as NavigationSmallStyles } from '@/styles/widgets/NavigationSmall/NavigationSmall.module.css';
+export { default as NavigationLargeStyles } from '@/styles/widgets/NavigationLarge/NavigationLarge.module.css';
