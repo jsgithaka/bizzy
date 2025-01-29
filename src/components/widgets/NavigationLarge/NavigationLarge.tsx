e9c501f6f65a9ae75083f54053ccd8c226315527
jsgithaka <jsgithaka@gmail.com>
2025-01-29 08:36:14 +0300

    feat(components/widgets): create and export `NavigationSmall` and `NavigationLarge` components

---
+++ src/components/widgets/NavigationLarge/NavigationLarge.tsx
@@ -0,0 +1,3 @@
+export default function NavigationLarge() {
+    return <div></div>;
+}
---
+++ src/exports/components/widgets.ts
@@ -0,0 +1,2 @@
+export { default as NavigationLarge } from '@/components/widgets/NavigationLarge/NavigationLarge';
+export { default as NavigationSmall } from '@/components/widgets/NavigationSmall/NavigationSmall';
