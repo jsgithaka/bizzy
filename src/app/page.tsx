8c2d07a6f8f55f91c7498c212a7f8c3e448e402a
jsgithaka <jsgithaka@gmail.com>
2025-01-29 11:52:07 +0300

    feat(app): update `Landing` page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 import { Page } from '@layoutComponents';
+import { NavigationItem, Button } from '@sharedComponents';
 
 export default function Landing() {
-    return <Page />;
+    return (
+        <Page
+            headerChildren={
+                <>
+                    <NavigationItem
+                        href='/'
+                        name='Landing'
+                        active
+                    />
+                    <NavigationItem
+                        href='/docs'
+                        name='Docs'
+                    />
+                    <Button
+                        href='/login'
+                        name='Login'
+                    />
+                </>
+            }
+            mainChildren={<></>}
+        />
+    );
 }
