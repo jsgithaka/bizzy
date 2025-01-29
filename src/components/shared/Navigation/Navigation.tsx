469d10cc23aa27b842effb18a9e5bca6824f79d3
jsgithaka <jsgithaka@gmail.com>
2025-01-29 08:54:02 +0300

    style(components/shared): add `Navigation` styles

--- src/components/shared/Navigation/Navigation.tsx
+++ src/components/shared/Navigation/Navigation.tsx
@@ -1,8 +1,9 @@
+import { NavigationStyles } from '@sharedStyles';
 import { NavigationLarge, NavigationSmall } from '@widgetsComponents';
 
 export default function Navigation({ children }: NavigationProps) {
     return (
-        <nav>
+        <nav className={NavigationStyles.Navigation}>
             <NavigationLarge>{children}</NavigationLarge>
             <NavigationSmall>{children}</NavigationSmall>
         </nav>
