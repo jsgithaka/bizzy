15ed6c0d8feddf2a24ea97060f1a579e420bc4d3
jsgithaka <jsgithaka@gmail.com>
2025-01-29 08:04:52 +0300

    feat(components/shared): create and `Navigation` component

--- src/exports/components/shared.ts
+++ src/exports/components/shared.ts
@@ -1,2 +1,3 @@
 export { default as Logo } from '@/components/shared/Logo/Logo';
 export { default as Brand } from '@/components/shared/Brand/Brand';
+export { default as Navigation } from '@/components/shared/Navigation/Navigation';
