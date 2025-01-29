72a5744211807eeec2414330598bf36c11bfc5e4
jsgithaka <jsgithaka@gmail.com>
2025-01-29 09:20:11 +0300

    feat(components/shared): add `NavigationItem` styles

--- src/components/shared/NavigationItem/NavigationItem.tsx
+++ src/components/shared/NavigationItem/NavigationItem.tsx
@@ -1,5 +1,13 @@
+import { NavigationItemStyles } from '@sharedStyles';
 import Link from 'next/link';
 
 export default function NavigationItem({ href, name }: NavigationItemProps) {
-    return <Link href={href}>{name}</Link>;
+    return (
+        <Link
+            className={NavigationItemStyles.NavigationItem}
+            href={href}
+        >
+            {name}
+        </Link>
+    );
 }
