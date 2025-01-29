1f6e15c94be2580cd6a94449cd6c25d38a907349
jsgithaka <jsgithaka@gmail.com>
2025-01-29 08:14:53 +0300

    feat(components/shared): create and export `NavigationItem` style module

--- src/exports/styles/shared.ts
+++ src/exports/styles/shared.ts
@@ -1,3 +1,4 @@
 export { default as LogoStyles } from '@/styles/shared/Logo/Logo.module.css';
 export { default as BrandStyles } from '@/styles/shared/Brand/Brand.module.css';
 export { default as NavigationStyles } from '@/styles/shared/Navigation/Navigation.module.css';
+export { default as NavigationItemStyles } from '@/styles/shared/NavigationItem/NavigationItem.module.css';
