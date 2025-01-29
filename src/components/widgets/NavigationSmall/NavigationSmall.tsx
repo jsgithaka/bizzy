b5fb9bd0e68b792887110972fa68584a6e2f653b
jsgithaka <jsgithaka@gmail.com>
2025-01-29 09:03:10 +0300

    style(components/widgets): add `NavigationSmall` styles

--- src/components/widgets/NavigationSmall/NavigationSmall.tsx
+++ src/components/widgets/NavigationSmall/NavigationSmall.tsx
@@ -1,6 +1,8 @@
+import { NavigationSmallStyles } from '@widgetsStyles';
+
 export default function NavigationSmall({ children }: NavigationSmallProps) {
     return (
-        <div>
+        <div className={NavigationSmallStyles.NavigationSmall}>
             <div></div>
             <div>{children}</div>
         </div>
