f337ff3d8f6c2149f1b17c55c7b70e01b14403c8
jsgithaka <jsgithaka@gmail.com>
2025-01-29 14:45:23 +0300

    style(components/views): add `SignIn` styles

--- src/components/views/SignIn/SignIn.tsx
+++ src/components/views/SignIn/SignIn.tsx
@@ -1,9 +1,28 @@
+import { Form, Input, Logo } from '@sharedComponents';
 import { View } from '@structuralComponents';
+import { SignInStyles } from '@viewsStyles';
 
 export default function SignIn() {
     return (
-        <View id='login'>
-            <></>
+        <View
+            id='login'
+            styles={SignInStyles.SignIn}
+        >
+            <Logo />
+            <Form value='Login'>
+                <Input
+                    type='email'
+                    label='Email'
+                    placeholder='your@email.com'
+                    id='email'
+                />
+                <Input
+                    type='password'
+                    label='Password'
+                    placeholder='********'
+                    id='password'
+                />
+            </Form>
         </View>
     );
 }
